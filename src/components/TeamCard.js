a38cb153930dd112e9f484d47b6f0c88920b412f
Conner Chappell <conner.chappell123@gmail.com>
2021-09-12 19:19:53 -0600

    Used map to create cards from data + basic styling

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -1,10 +1,19 @@
 import '../App.css'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
 
-const TeamCard = () => {
+const TeamCard = (props) => {
+    const { teamID, teamBadge, teamName } = props
     return (
-        <div>
-
-        </div>
+        <Card key={teamID} className="card">
+            <img src={teamBadge} alt="Team Badge"/>
+            <CardContent>
+                <Typography variant="h4" component="h2">
+                    {teamName}
+                </Typography>
+            </CardContent>
+        </Card>
     )
 }
 
--- src/components/TeamContainer.js
+++ src/components/TeamContainer.js
@@ -1,10 +1,18 @@
-import { teams } from "../data/teams";
-import TeamCard from "./TeamCard";
+import { teams } from '../data/teams'
+import TeamCard from './TeamCard'
 
 const TeamContainer = () => {
     return (
-        <div className="senator-container">
-
+        <div className="team-container">
+            {teams.map((team) => {
+                return (
+                    <TeamCard
+                        teamID= {team.idTeam}
+                        teamBadge={team.strTeamBadge}
+                        teamName={team.strTeam}
+                    />
+                )
+            })}
         </div>
     )
 }
